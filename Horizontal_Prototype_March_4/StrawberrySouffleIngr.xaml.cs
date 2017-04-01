ee831505c668f10cf8ff45c75a087481b8dd7ce0
rlkbelt <ryankonynenbelt@gmail.com>
2017-04-01 13:46:21 -0600

    Fixed souffle

--- Horizontal_Prototype_March_4/StrawberrySouffleIngr.xaml.cs
+++ Horizontal_Prototype_March_4/StrawberrySouffleIngr.xaml.cs
@@ -10,23 +10,48 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Horizontal_Prototype_March_4
 {
-	/// <summary>
-	/// Interaction logic for StrawberrySouffleIngr.xaml
-	/// </summary>
-	public partial class StrawberrySouffleIngr : Window
-	{
-		public StrawberrySouffleIngr()
-		{
-			InitializeComponent();
-		}
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class StrawberrySouffleIngr : UserControl
+    {
+        MainWindow window;
+        public StrawberrySouffleIngr()
+        {
+            InitializeComponent();
+            this.Loaded += (s, e) =>
+            {
+                window = Window.GetWindow(this) as MainWindow;
+            };
+        }
 
-		private void straw_quan1_TextChanged(object sender, TextChangedEventArgs e)
-		{
+        public void SliderMover(object sender, RoutedEventArgs e)
+        { }
 
-		}
-	}
+        private void BackClick(object sender, RoutedEventArgs e)
+        {
+            if (window.backStack.Peek() is HomePage)
+            {
+                window.expanderInvisible();
+            }
+            window.CurrentUserControl = window.backStack.Pop();
+            window._Navigation.Navigate(window.CurrentUserControl);
+        }
+
+        private void print_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            printDialog.Show();
+            printDialog.update();
+
+
+
+
+        }
+    }
 }
